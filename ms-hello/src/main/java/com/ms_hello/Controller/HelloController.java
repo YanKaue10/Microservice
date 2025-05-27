1f94bbb1d974b33f42be1eca986498e3e78d3859
Yan <yankaue4@gmail.com>
2025-05-27 19:02:26 -0300

    Primeiro commit do projeto Ms-Trabalho

---
+++ ms-hello/src/main/java/com/ms_hello/Controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ms_hello.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "Hello Microservice";
+    }
+}
---
+++ ms-hello/src/test/java/com/ms_hello/MsHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
